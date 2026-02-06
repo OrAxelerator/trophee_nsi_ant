0080be77504797983faef5241c5b002b7e5b1419
OrAxelerator <axel.piat@icloud.com>
2026-02-06 09:40:07 +0100

    typage fonction read_world + fourmi se déplace en diagonale devant mais plus sur les cotés

--- cellule.py
+++ cellule.py
@@ -3,8 +3,8 @@ from read_world import read_world
 
 
 ant = { # exemple
-    "pos" : (0, 0),
-    "angle" : (0,1),
+    "pos" : (3, 3),
+    "angle" : (0,-1),
     "have_food" : False
 }
 
@@ -12,29 +12,48 @@ def get_cellule(world, ant: dict) -> list:
     """renvoi la liste des positions possibles autour de la fourmi"""
     choices = {
         "up": (-1,0),
+        "up-right": (-1,1),
+        "up-left": (-1,-1),
         "down" :(1,0),
+        "down-right" :(1,1),
+        "down-left" :(1,-1),
         "left" :(0,-1),
-        "right" : (0,1)
+        "right" : (0,1),
     }
 
     interdiction=[]
     if ant["pos"][0] == 0:
         interdiction.append(choices["up"])
+        interdiction.append(choices["up-right"])
+        interdiction.append(choices["up-left"])
     if ant["pos"][0] == len(world[0]) - 1 :
         interdiction.append(choices["down"])
+        interdiction.append(choices["down-left"])
+        interdiction.append(choices["down-right"])
     if ant["pos"][1] == 0 :
         interdiction.append(choices["left"])
+        interdiction.append(choices["down-left"])
+        interdiction.append(choices["up-left"])
     if ant["pos"][1] == len(world[0]) - 1:
         interdiction.append(choices["right"])
+        interdiction.append(choices["up-right"])
+        interdiction.append(choices["down-right"])
+
 
     possibility = []
+    coef = 0 # indiceou se situe le 1 ou -1 dans angle
+    if ant["angle"][0] == -1 or ant["angle"][0] == 1:
+        coef = 0
+    elif ant["angle"][1] == -1 or ant["angle"][1] == 1:
+        coef = 1
+
     for el in choices:
         if choices[el] not in interdiction:
-
-            if read_world(ant, choices[el]) != "X" :
-                possibility.append(choices[el])
+            if (choices[el][coef] == ant["angle"][coef]):
+                if read_world(ant, choices[el]) != "X" :
+                    possibility.append(choices[el])
 
     return possibility
 
 
-#print(get_cellule(espace, ant)) #test
+print(get_cellule(espace, ant)) #test
--- read_world.py
+++ read_world.py
@@ -1,6 +1,6 @@
 from world import espace
 
-def read_world(ant, direction):
+def read_world(ant:dict, direction:tuple | dict):
     """
     calcule la position de ant + direction
     in : tuple or array
